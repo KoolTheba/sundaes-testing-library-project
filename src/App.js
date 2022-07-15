84c2cbcbd396454f7042c232d84f4582bacbb90a
KoolTheba <gomez.teba@gmail.com>
2022-07-15 18:03:42 +0200

    added basic styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-teal-100 p-8 m-10">
+      <p className="text-3xl text-gray-800 font-bold mb-5">Welcome!</p>
+      <p className="text-gray-800 text-lg">React and Tailwind CSS in action</p>
     </div>
   );
 }
