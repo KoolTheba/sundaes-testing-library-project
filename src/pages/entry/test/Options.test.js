e1f2fb7cee03435c3d5dbfb87a1f04510caa2204
KoolTheba <gomez.teba@gmail.com>
2022-07-17 18:11:14 +0200

    added tests for options and alert banner

---
+++ src/pages/entry/test/Options.test.js
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  // find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // confirm alt text of images
+  const altTexts = scoopImages.map((img) => img.alt);
+  expect(altTexts).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("displays image for each topping option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  const altTexts = toppingImages.map((img) => img.alt);
+  expect(altTexts).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
