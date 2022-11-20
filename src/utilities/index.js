972b172b2cc548b13b03e42b484d1b5c29f435f3
KoolTheba <gomez.teba@gmail.com>
2022-11-20 21:08:05 +0100

    feat: added format currency utility

---
+++ src/utilities/index.js
@@ -0,0 +1,7 @@
+export const formatCurrency = (currency) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(currency);
+};
