aa4f08caf78b5f2cef2f740542f36bca07d3133c
KoolTheba <gomez.teba@gmail.com>
2022-12-06 12:37:48 +0100

    feat: added checkbox for toppings options

--- src/pages/entry/ToppingOption.js
+++ src/pages/entry/ToppingOption.js
@@ -4,21 +4,18 @@ export default function ToppingOption({ name, image }) {
   const { updateItemCount } = useOrderDetails();
 
   const handleChange = (e) =>
-    updateItemCount(name, Number(e.target.value), "scoops");
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
 
   return (
     <li key={name}>
       <h1>{name}</h1>
       <img src={`http://localhost:3030/${image}`} alt={`${name} topping`} />
-      <form>
-        <label htmlFor={name}>{name}</label>
-        <input
-          id={name}
-          type="number"
-          defaultValue={0}
-          onChange={handleChange}
-        />
-      </form>
+      <div>
+        <label htmlFor={name}>
+          <input id={name} type="checkbox" onChange={handleChange} />
+          {name}
+        </label>
+      </div>
     </li>
   );
 }
