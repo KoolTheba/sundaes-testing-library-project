e3567b23881d331edb1a77519289d6b555e4cc0f
KoolTheba <gomez.teba@gmail.com>
2022-12-06 12:38:04 +0100

    feat: added grand total in OrderEntry component

--- src/pages/entry/OrderEntry.js
+++ src/pages/entry/OrderEntry.js
@@ -1,10 +1,27 @@
+// component
 import Options from "./Options";
 
+// utility
+import { formatCurrency } from "../../utilities";
+
+// custom hook
+import { useOrderDetails } from "../../context/OrderDetails";
+
 export default function OrderEntry() {
+  const { totals } = useOrderDetails();
+  const grandTotal = formatCurrency(totals.scoops + totals.toppings);
   return (
-    <div>
-      <Options optionType={"scoops"} />
-      <Options optionType={"toppings"} />
-    </div>
+    <>
+      <div>
+        <Options optionType={"scoops"} />
+        <Options optionType={"toppings"} />
+      </div>
+      <div>
+        <h2>
+          Grand total:
+          {grandTotal}
+        </h2>
+      </div>
+    </>
   );
 }
