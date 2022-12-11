d5e542f7ae1d7991705fd4bb7c1493072b5a8e01
KoolTheba <gomez.teba@gmail.com>
2022-12-11 12:34:06 +0100

    test: added test for latest scoops amount validation

--- src/pages/entry/test/Options.test.js
+++ src/pages/entry/test/Options.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "../../../test-utils/testing-library-utils";
-
+import userEvent from "@testing-library/user-event";
 import Options from "../Options";
 
 test("displays image for each scoop option from server", async () => {
@@ -28,4 +28,24 @@ test("displays image for each topping option from server", async () => {
     "M&Ms topping",
     "Hot fudge topping",
   ]);
+});
+
+test("don't update total if scoops input is invalid", async () => {
+  render(<Options optionType="scoops" />);
+  const user = userEvent.setup();
+
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  expect(vanillaInput).toBeInTheDocument();
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1.5");
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // tries to add negative amounts
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "-1");
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
 });
