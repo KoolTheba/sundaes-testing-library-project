404d7e63394e8ed7139e41dc1631ce83ca7e5a85
KoolTheba <gomez.teba@gmail.com>
2022-12-11 11:35:06 +0100

    feat: added mock handler for order post call

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -33,4 +33,7 @@ export const handlers = [
       ])
     );
   }),
+  rest.post("http://localhost:3030/order", (req, res, ctx) => {
+    return res(ctx.json({ orderNumber: 7984796200 }));
+  }),
 ];
