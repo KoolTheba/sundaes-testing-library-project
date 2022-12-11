4cdf3bb739cd931fbf311d20501e98babd25e866
KoolTheba <gomez.teba@gmail.com>
2022-12-11 12:18:01 +0100

    test: added test for order button disabled

--- src/pages/entry/test/OrderEntry.test.js
+++ src/pages/entry/test/OrderEntry.test.js
@@ -3,9 +3,33 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
-import OrderEntry from "../OrderEntry";
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
+import OrderEntry from "../OrderEntry";
+
+test("Place order button is disabled when no scoops are selected", async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+  const user = userEvent.setup();
+
+  const orderButton = screen.getByRole("button", { name: /place order/i });
+  expect(orderButton).toBeDisabled();
+
+  // add ice cream scoop ancd check button is enabled
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  expect(vanillaInput).toBeInTheDocument();
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  // add ice cream scoop ancd check button is disabled
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
+});
 
 test("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
