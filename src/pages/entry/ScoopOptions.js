9b257d6199f69a792d1d360416fe90a0654cc068
KoolTheba <gomez.teba@gmail.com>
2022-12-11 12:33:48 +0100

    feat: added amount validation for scoops selection

--- src/pages/entry/ScoopOptions.js
+++ src/pages/entry/ScoopOptions.js
@@ -3,6 +3,7 @@ import { useOrderDetails } from "../../context/OrderDetails";
 
 export default function ScoopOptions({ name, image }) {
   const { updateItemCount, optionCounts } = useOrderDetails();
+  const [isAmountValid, setIsAmountValid] = useState(false);
   const [amount, setAmount] = useState(0);
 
   const scoopsAmountRef = useCallback(
@@ -15,8 +16,18 @@ export default function ScoopOptions({ name, image }) {
   );
 
   const handleChange = (e) => {
-    setAmount(e.target.value);
-    updateItemCount(name, Number(e.target.value), "scoops");
+    const currentValue = e.target.value;
+    const currentValueFloat = parseFloat(currentValue);
+    const valueIsValid =
+      0 <= currentValueFloat &&
+      currentValueFloat <= 10 &&
+      Math.floor(currentValueFloat) === currentValueFloat;
+
+    setIsAmountValid(valueIsValid);
+
+    isAmountValid && setAmount(currentValue);
+    const newValue = valueIsValid ? parseInt(currentValue) : 0;
+    updateItemCount(name, newValue, "scoops");
   };
 
   return (
@@ -31,7 +42,7 @@ export default function ScoopOptions({ name, image }) {
           type="number"
           value={amount}
           min={0}
-          max={100}
+          max={10}
           onChange={handleChange}
           ref={scoopsAmountRef}
         />
