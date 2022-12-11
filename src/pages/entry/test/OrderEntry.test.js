725419e82205b39cf2d331ff53a5ed4ae9a39a47
KoolTheba <gomez.teba@gmail.com>
2022-12-11 11:36:39 +0100

    test: added jest.fn as prop

--- src/pages/entry/test/OrderEntry.test.js
+++ src/pages/entry/test/OrderEntry.test.js
@@ -17,7 +17,7 @@ test("handles error for scoops and toppings routes", async () => {
     )
   );
 
-  render(<OrderEntry />);
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
   await waitFor(async () => {
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
