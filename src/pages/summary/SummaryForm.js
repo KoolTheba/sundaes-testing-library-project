c72d3e7ff5304f5d3fb414751526d9bb69d1afa4
KoolTheba <gomez.teba@gmail.com>
2022-12-11 11:32:59 +0100

    feat: removed popover effect and added phases logic onClick

--- src/pages/summary/SummaryForm.js
+++ src/pages/summary/SummaryForm.js
@@ -1,43 +1,49 @@
 import React from "react";
 
-const SummaryForm = () => {
+// constants
+import { ORDER_PHASES } from "../../constants/index";
+
+const SummaryForm = ({ setOrderPhase }) => {
   const [tcChecked, setTcChecked] = React.useState(false);
-  const [popoverDisplayed, setPopoverDisplayed] = React.useState(false);
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    setOrderPhase(ORDER_PHASES.COMPLETE);
+  };
 
   return (
-    <div className="form__wrapper">
-      <form>
-        <input
-          onChange={(e) => setTcChecked(e.target.checked)}
-          type="checkbox"
-          checked={tcChecked}
-          aria-checked={tcChecked}
-          id="terms-and-conditions-checkbox"
-        />
-        <label
-          className="tyc__label"
-          htmlFor="terms-and-conditions-checkbox"
-          onMouseOver={() => setPopoverDisplayed(true)}
-          onMouseOut={() => setPopoverDisplayed(false)}
-        >
-          I agree on terms and conditions
-        </label>
-        {popoverDisplayed && (
-          <div className="tyc__popover">No icecream will be delivered</div>
-        )}
-        <button
-          className="confirm__btn"
-          type="submit"
-          disabled={!tcChecked}
-          onClick={() => console.log("I've been clicked!")}
-          style={{
-            backgroundColor: tcChecked ? "teal" : "grey",
-          }}
-        >
-          Confirm your order
+    <>
+      <div className="form__wrapper">
+        <form>
+          <input
+            onChange={(e) => setTcChecked(e.target.checked)}
+            type="checkbox"
+            checked={tcChecked}
+            aria-checked={tcChecked}
+            id="terms-and-conditions-checkbox"
+          />
+          <label className="tyc__label" htmlFor="terms-and-conditions-checkbox">
+            I agree on terms and conditions
+          </label>
+          <button
+            className="confirm__btn"
+            type="submit"
+            disabled={!tcChecked}
+            onClick={handleSubmit}
+            style={{
+              backgroundColor: tcChecked ? "teal" : "grey",
+            }}
+          >
+            Confirm your order
+          </button>
+        </form>
+      </div>
+      <div>
+        <button onClick={() => setOrderPhase(ORDER_PHASES.IN_PROGRESS)}>
+          Back to your order
         </button>
-      </form>
-    </div>
+      </div>
+    </>
   );
 };
 
