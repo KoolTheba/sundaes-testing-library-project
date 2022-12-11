bd43c7a8755245fe26b87b678983ff77890d086c
KoolTheba <gomez.teba@gmail.com>
2022-12-11 11:32:00 +0100

    feat: added conditional render to App depending on process phases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { OrderDetailsProvider } from "./context/OrderDetails";
 import OrderEntry from "./pages/entry/OrderEntry";
+import OrderSummary from "./pages/summary/OrderSummary";
+import OrderConfirmation from "./pages/confirmation/OrderConfirmation";
+import { ORDER_PHASES } from "./constants/index";
 import "./index.css";
 
 function App() {
+  const [orderPhase, setOrderPhase] = useState(ORDER_PHASES.IN_PROGRESS);
   return (
     <>
       <OrderDetailsProvider>
-        <OrderEntry />
+        {orderPhase === ORDER_PHASES.IN_PROGRESS && (
+          <OrderEntry setOrderPhase={setOrderPhase} />
+        )}
+        {orderPhase === ORDER_PHASES.IN_REVIEW && (
+          <OrderSummary setOrderPhase={setOrderPhase} />
+        )}
+        {orderPhase === ORDER_PHASES.COMPLETE && (
+          <OrderConfirmation setOrderPhase={setOrderPhase} />
+        )}
       </OrderDetailsProvider>
     </>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,3 +2,9 @@ export const pricePerItem = {
   scoops: 2,
   toppings: 1.5,
 };
+
+export const ORDER_PHASES = {
+  IN_PROGRESS: "inProgress",
+  IN_REVIEW: "inReview",
+  COMPLETE: "complete",
+};
