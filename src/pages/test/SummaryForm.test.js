ea6c6757ce5ff6f9ff2d1f0375849f1e06f3cfa6
KoolTheba <gomez.teba@gmail.com>
2022-12-11 11:33:15 +0100

    test: adapted tests after changes on component

--- src/pages/test/SummaryForm.test.js
+++ src/pages/test/SummaryForm.test.js
@@ -36,25 +36,3 @@ test("button should be enabled if checkbox is checked and disabled on second cli
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
-
-test.skip("popover responds to label hover", async () => {
-  const user = userEvent.setup();
-  render(<SummaryForm />);
-
-  // popover starts out hidden
-  const nullPopover = screen.queryByText(/no icecream will be delivered/i);
-  expect(nullPopover).not.toBeInTheDocument();
-
-  // popover appears upon mouseover of checkbox label
-  const tycLabelElement = screen.getByLabelText(
-    /I agree on terms and conditions/i
-  );
-  await user.hover(tycLabelElement);
-  const popover = screen.getByText(/no icecream will be delivered/i);
-  expect(popover).toBeInTheDocument();
-
-  // popover disappears when we mouse out
-  await user.unhover(tycLabelElement);
-  const visiblePopover = screen.queryByText(/no icecream will be delivered/i);
-  expect(visiblePopover).not.toBeInTheDocument();
-});
